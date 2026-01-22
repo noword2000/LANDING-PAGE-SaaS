770a6a5c2568079e08ae17c66798d804f8f624e4
El Hadj Abdoul Aziz Ba <baabdoulaziz00@gmail.com>
2026-01-22 14:38:49 +0100

    chore: mise en place des outils de qualité de code et de formatage
    - Installation des dépendances de développement : htmlhint, stylelint, eslint et prettier.
    - Création des fichiers de configuration personnalisés (.htmlhintrc, stylelint.config.mjs, eslint.config.mjs, .prettierrc).
    - Mise en conformité du code avec les bonnes pratiques.
    - Formatage global du projet (HTML, SCSS, JS) pour une cohérence parfaite.
    
    Cet environnement garantit un code propre, lisible et maintenable, facilitant la collaboration et la robustesse du projet SaaS.

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    rules: {
+      eqeqeq: "error", //Force l'usage de l'égalité triple === au lieu de la double ==.
+      "no-var": "error", //Interdit totalement l'utilisation du mot-clé var. Tu dois utiliser let ou const.
+      "prefer-arrow-callback": "error", //Oblige l'utilisation des fonctions fléchées () => { ... } pour les fonctions de rappel (callbacks).
+      quotes: ["error", "double"], //Force l'utilisation des doubles guillemets (" ") pour toutes tes chaînes de caractères.
+      semi: ["error", "always"], //Force la présence du point-virgule
+    },
+  },
+]);
